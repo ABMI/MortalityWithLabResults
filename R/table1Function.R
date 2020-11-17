8184ed796b9fc6e710f273c2d6d4be8254ab561c
JaehyeongCho <boyinai03@gmail.com>
2020-11-17 17:56:06 +0900

    modify table1Function 1/2

--- R/table1Function.R
+++ R/table1Function.R
@@ -41,7 +41,9 @@ table1Function <- function (cdmDatabaseSchema,
 
   covariateSettings <- list()
   covariateSettings <- FeatureExtraction::createDefaultCovariateSettings()
-  Table1_30days <- PatientLevelPrediction::getPlpTable(cdmDatabaseSchema = cdmDatabaseSchema,
+  covariateSettings$MeasurementValueShortTerm = TRUE
+  covariateSettings$shortTermStartDays <- -3
+  Table1_30days <- inHospitalgetPlpTable(cdmDatabaseSchema = cdmDatabaseSchema,
                                oracleTempSchema = oracleTempSchema,
                                covariateSettings = covariateSettings,
                                longTermStartDays = -365,
@@ -64,7 +66,9 @@ table1Function <- function (cdmDatabaseSchema,
 
   covariateSettings <- list()
   covariateSettings <- FeatureExtraction::createDefaultCovariateSettings()
-  Table1_90days <- PatientLevelPrediction::getPlpTable(cdmDatabaseSchema = cdmDatabaseSchema,
+  covariateSettings$MeasurementValueShortTerm = TRUE
+  covariateSettings$shortTermStartDays <- -3
+  Table1_90days <- inHospitalgetPlpTable(cdmDatabaseSchema = cdmDatabaseSchema,
                                oracleTempSchema = oracleTempSchema,
                                covariateSettings = covariateSettings,
                                longTermStartDays = -365,
@@ -77,95 +81,84 @@ table1Function <- function (cdmDatabaseSchema,
 }
 
 
-# {
-#   covariateSettings <- FeatureExtraction::createCovariateSettings(useDemographicsGender = T)
-#
-#   plpData <- PatientLevelPrediction::getPlpData(connectionDetails,
-#                                                 cdmDatabaseSchema = cdmDatabaseSchema,
-#                                                 cohortId = 356, outcomeIds = 355,
-#                                                 cohortDatabaseSchema = cohortDatabaseSchema,
-#                                                 outcomeDatabaseSchema = cohortDatabaseSchema,
-#                                                 cohortTable = cohortTable,
-#                                                 outcomeTable = cohortTable,
-#                                                 covariateSettings=covariateSettings)
-#
-#   population <- PatientLevelPrediction::createStudyPopulation(plpData = plpData,
-#                                                               outcomeId = 355,
-#                                                               binary = T,
-#                                                               includeAllOutcomes = T,
-#                                                               requireTimeAtRisk = T,
-#                                                               minTimeAtRisk = 364,
-#                                                               riskWindowStart = 1,
-#                                                               riskWindowEnd = 365,
-#                                                               removeSubjectsWithPriorOutcome = T)
-#
-#   if (missing(cdmDatabaseSchema))
-#     stop("Need to enter cdmDatabaseSchema")
-#   if (missing(population))
-#     stop("Need to enter population")
-#   if (missing(connectionDetails))
-#     stop("Need to enter connectionDetails")
-#   if (class(population) != "data.frame")
-#     stop("wrong population class")
-#   if (sum(c("cohortId", "subjectId", "cohortStartDate") %in%
-#           colnames(population)) != 3)
-#     stop("population missing required column")
-#   if (sum(population$outcomeCount > 0) == 0)
-#     stop("No outcomes")
-#   if (sum(population$outcomeCount == 0) == 0)
-#     stop("No non-outcomes")
-#   connection <- DatabaseConnector::connect(connectionDetails)
-#   popCohort <- population[population$outcomeCount == 0, c("cohortId",
-#                                                           "subjectId", "cohortStartDate", "cohortStartDate")]
-#   colnames(popCohort)[4] <- "cohortEndDate"
-#   colnames(popCohort) <- SqlRender::camelCaseToSnakeCase(colnames(popCohort))
-#   cohortTable <- "#temp_person"
-#   DatabaseConnector::insertTable(connection = connection,
-#                                  tableName = cohortTable, data = popCohort, tempTable = T)
-#   settings <- list()
-#   settings$covariateSettings <- FeatureExtraction::createCovariateSettings(useDemographicsGender = T,
-#                                                                              useDemographicsAge = T, useDemographicsAgeGroup = T,
-#                                                                              useDemographicsRace = T, useDemographicsEthnicity = T,
-#                                                                              useConditionGroupEraLongTerm = T, useDrugGroupEraLongTerm = T,
-#                                                                              useCharlsonIndex = T, useChads2Vasc = T, useDcsi = T,
-#                                                                              useProcedureOccurrenceShortTerm = T, useMeasurementValueShortTerm = T, shortTermStartDays = -3,
-#                                                                              longTermStartDays = -365)
-#   settings$aggregated <- T
-#   settings$cdmDatabaseSchema <- cdmDatabaseSchema
-#   if (!missing(oracleTempSchema)) {
-#     settings$oracleTempSchema <- oracleTempSchema
-#   }
-#   settings$cohortTable <- cohortTable
-#   settings$cohortId <- -1
-#   #settings$cohortTableIsTemp <- T
-#   settings$connection <- connection
-#
-#   covariateData1 <- do.call(FeatureExtraction::getDbDefaultCovariateData, settings)
-#
-#   popCohort <- population[population$outcomeCount > 0, c("cohortId",
-#                                                          "subjectId", "cohortStartDate", "cohortStartDate")]
-#   colnames(popCohort)[4] <- "cohortEndDate"
-#   colnames(popCohort) <- SqlRender::camelCaseToSnakeCase(colnames(popCohort))
-#   DatabaseConnector::insertTable(connection = connection,
-#                                  tableName = cohortTable, data = popCohort, tempTable = T)
-#   covariateData2 <- do.call(FeatureExtraction::getDbDefaultCovariateData,
-#                             settings)
-#   fileName <- system.file("csv", "Table1Specs.csv", package = "MortalityWithLabResults")
-#   tabSpec <-  read.csv(fileName, stringsAsFactors = FALSE)
-#   tabSpec <- rbind(tabSpec, c(label = "Age in years", analysisId = 2,
-#                               covariateIds = 1002))
-#   tab1 <- FeatureExtraction::createTable1(covariateData1 = covariateData1,
-#                                           covariateData2 = covariateData2, specifications = tabSpec,
-#                                           output = "two columns")
-#   #return(tab1)
-#
-#
-#   #Save RDS
-#   ParallelLogger::logInfo(paste0("table1 result save in ",
-#                                  file.path(outputLocation, "table1Result.rds")))
-#   saveRDS(tab1, file.path(outputLocation, "table1Result.rds"))
-# }
-#
-#
-# # df <- ff::as.ram(covariateData1$covariates)
-# # df[substr(df$covariateId, nchar(df$covariateId)-2, nchar(df$covariateId))=="504",]
+
+
+inHospitalgetPlpTable <- function (cdmDatabaseSchema, oracleTempSchema, covariateSettings,
+          longTermStartDays = -365, population, connectionDetails,
+          cohortTable = "#temp_person")
+{
+  if (missing(cdmDatabaseSchema))
+    stop("Need to enter cdmDatabaseSchema")
+  if (missing(population))
+    stop("Need to enter population")
+  if (missing(connectionDetails))
+    stop("Need to enter connectionDetails")
+  if (class(population) != "data.frame")
+    stop("wrong population class")
+  if (sum(c("subjectId", "cohortStartDate") %in% colnames(population)) !=
+      2)
+    stop("population missing required column")
+  if (sum(population$outcomeCount > 0) == 0)
+    stop("No outcomes")
+  if (sum(population$outcomeCount == 0) == 0)
+    stop("No non-outcomes")
+  connection <- DatabaseConnector::connect(connectionDetails)
+  popCohort <- population[population$outcomeCount == 0, c("subjectId",
+                                                          "cohortStartDate", "cohortStartDate")]
+  popCohort <- data.frame(cohortId = -1, popCohort)
+  colnames(popCohort)[4] <- "cohortEndDate"
+  colnames(popCohort) <- SqlRender::camelCaseToSnakeCase(colnames(popCohort))
+  DatabaseConnector::insertTable(connection = connection,
+                                 tableName = cohortTable, data = popCohort, tempTable = T)
+  settings <- list()
+  if (!missing(covariateSettings)) {
+    settings$covariateSettings <- covariateSettings
+  }
+  else {
+    settings$covariateSettings <- FeatureExtraction::createCovariateSettings(useDemographicsGender = T,
+                                                                             useDemographicsAge = T, useDemographicsAgeGroup = T,
+                                                                             useDemographicsRace = T, useDemographicsEthnicity = T,
+                                                                             useConditionGroupEraLongTerm = T, useDrugGroupEraLongTerm = T,
+                                                                             useCharlsonIndex = T, useChads2Vasc = T, useDcsi = T,
+                                                                             longTermStartDays = longTermStartDays)
+  }
+  settings$aggregated <- T
+  settings$cdmDatabaseSchema <- cdmDatabaseSchema
+  if (!missing(oracleTempSchema)) {
+    settings$oracleTempSchema <- oracleTempSchema
+  }
+  settings$cohortTable <- cohortTable
+  settings$cohortId <- -1
+  settings$cohortTableIsTemp <- T
+  settings$connection <- connection
+  covariateData1 <- do.call(FeatureExtraction::getDbCovariateData,
+                            settings)
+  popCohort <- population[population$outcomeCount > 0, c("subjectId",
+                                                         "cohortStartDate", "cohortStartDate")]
+  popCohort <- data.frame(cohortId = -1, popCohort)
+  colnames(popCohort)[4] <- "cohortEndDate"
+  colnames(popCohort) <- SqlRender::camelCaseToSnakeCase(colnames(popCohort))
+  DatabaseConnector::insertTable(connection = connection,
+                                 tableName = cohortTable, data = popCohort, tempTable = T)
+  covariateData2 <- do.call(FeatureExtraction::getDbCovariateData,
+                            settings)
+  tabSpec <- inHospitalgetDefaultTable1Specifications()
+  tabSpec <- rbind(tabSpec, c(label = "Age in years", analysisId = 2,
+                              covariateIds = 1002))
+  tab1 <- FeatureExtraction::createTable1(covariateData1 = covariateData1,
+                                          covariateData2 = covariateData2, specifications = tabSpec,
+                                          output = "two columns")
+  return(tab1)
+}
+
+
+
+inHospitalgetDefaultTable1Specifications <- function ()
+{
+  fileName <- system.file("csv", "Table1Specs.csv", package = "MortalityWithLabResults")
+  colTypes <- list(label = readr::col_character(), analysisId = readr::col_integer(),
+                   covariateIds = readr::col_character())
+  specifications <- readr::read_csv(fileName, col_types = colTypes,
+                                    guess_max = )
+  return(specifications)
+}
